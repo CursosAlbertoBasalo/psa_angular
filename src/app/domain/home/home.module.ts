47a973e5c236e28648c1d990dac164ad88a70988
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-23 15:52:39 +0100

    feat: homew works

---
+++ src/app/domain/home/home.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DataModule } from 'src/app/shared/data/data.module';
+import { UiModule } from 'src/app/shared/ui/ui.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { CategoriesComponent } from './ui/categories/categories.component';
+
+@NgModule({
+  declarations: [HomeComponent, CategoriesComponent],
+  imports: [CommonModule, HomeRoutingModule, UiModule, DataModule],
+})
+export class HomeModule {}
---
+++ src/app/domain/home/ui/categories/categories.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoriesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
